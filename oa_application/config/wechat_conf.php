59a4f18ecd9159bd7e7da448af258aa79d53d362
cheaboar <826802085@qq.com>
2015-07-21 16:09:07 +0800

    微信菜单添加配置

--- oa_application/config/wechat_conf.php
+++ oa_application/config/wechat_conf.php
@@ -10,53 +10,84 @@ $config['token'] = 'Yijianyi';
 $config['secret'] = 'b4843edb87fc8e3f0b1034bf946c5189';
 $config['encodingAESKey'] = 'OvXZAh5akCf9oxrwgLbsu4a61gw4KmuVdGwZzxa1Cjx'; // 可选
 
+$config['service'] = array(
+    'appId' => 'wx86b3751ad43f4062',
+    'secret' => 'f52a587fefed285df9244f310eee8a34',
+);
+
+
+
 
 $config['subcribe_menus'] = array(
     array(
-        'name' => '服务',
-        'type' => 'view',
-        'key' => 'http://subcribe.ecare-easy.com/service/wechat/service_info',
-        'buttons' => null
+        'name' => '服务预约',
+        'type' => 'null',
+        'key' => 'null',
+        'buttons' => array(
+            array(
+                'name' => '月嫂',
+                'type' => 'view',
+                'key' => 'http://subcribe.ecare-easy.com/Service/wechat/service_detail?id=1',
+                'buttons' => null,
+            ),
+            array(
+                'name' => '医院陪护',
+                'type' => 'view',
+                'key' => 'http://subcribe.ecare-easy.com/Service/wechat/service_detail?id=3',
+                'buttons' => null,
+            ),
+            array(
+                'name' => '居家照护',
+                'type' => 'view',
+                'key' => 'http://subcribe.ecare-easy.com/Service/wechat/service_detail?id=2',
+                'buttons' => null,
+            ),
+        )
     ),
-//    array(
-//        'name' => '菜单',
-//        'key' => null,
-//        'type' => null,
-//        'buttons' => array(
-//            array(
-//                'name' => '服务',
-//                'type' => 'view',
-//                'key' => 'http://www.baidu.com',
-//                'buttons' => null
-//            ),
-//            array(
-//                'name' => '百度',
-//                'type' => 'view',
-//                'key' => 'http://www.baidu.com',
-//                'buttons' => null
-//            )
-//
-//        )
-//    ),
-//    array(
-//        'name' => '菜单1',
-//        'key' => null,
-//        'type' => null,
-//        'buttons' => array(
-//            array(
-//                'name' => '服务1',
-//                'type' => 'view',
-//                'key' => 'http://www.baidu.com',
-//                'buttons' => null
-//            ),
-//            array(
-//                'name' => '百度1',
-//                'type' => 'view',
-//                'key' => 'http://www.baidu.com',
-//                'buttons' => null
-//            )
-//
-//        )
-//    )
+    array(
+        'name' => '健康知识',
+        'type' => 'view',
+        'key' => 'http://subcribe.ecare-easy.com/knowledge/index/knowledge',
+        'buttons' => null,
+    ),
+    array(
+        'name'      => '我',
+        'type'      => null,
+        'key'       => null,
+        'buttons'   => array(
+            array(
+                'name'      => '个人中心',
+                'type'      => 'view',
+                'key'      => 'http://subcribe.ecare-easy.com/Service/wechat/user_center',
+                'buttons'      => null,
+            ),
+            array(
+                'name'      => '我的预约',
+                'type'      => 'view',
+                'key'      => 'http://subcribe.ecare-easy.com/Service/wechat/my_appointment',
+                'buttons'      => null,
+            ),
+            array(
+                'name'      => '服务查询',
+                'type'      => 'view',
+                'key'      => 'http://subcribe.ecare-easy.com/Service/wechat/my_signed_appointment.html',
+                'buttons'      => null,
+            ),
+            array(
+                'name'      => '吐槽建议',
+                'type'      => 'view',
+                'key'      => 'http://subcribe.ecare-easy.com/Service/wechat/advice.html',
+                'buttons'      => null,
+            ),
+            array(
+                'name'      => '关于一家依',
+                'type'      => 'view',
+                'key'      => 'http://subcribe.ecare-easy.com/Service/wechat/about_us',
+                'buttons'      => null,
+            ),
+
+        ),
+    )
+
 );
 
