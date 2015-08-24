f28060976f1e27a8a4b705d11ac3d09a67ba522a
cheaboar <826802085@qq.com>
2015-08-24 10:33:29 +0800

    预约服务管理修改

--- oa_application/models/oa_appointment.php
+++ oa_application/models/oa_appointment.php
@@ -24,14 +24,14 @@ class OA_Appointment extends CI_Model
 
 
         $this->db->select('s.id, s.confirm_time, s.contact_time, s.state,s.easy_time, a1.area_name as provinceName, a2.area_name as cityName, a3.area_name as areaName, a.address,
-        u.user_name as name, service_info.name as service_name, u.user_phone as phone, service_type');
+        s.name as name, s.phone as phone, service_type, s.create_time');
         $this->db->from('yjy_service_appointment as s');
         $this->db->join('oa_address as a', 's.address_id = a.address_id', 'left');
         $this->db->join('oa_areas as a1', 'a.province = a1.area_id', 'left');
         $this->db->join('oa_areas as a2', 'a.city = a2.area_id', 'left');
         $this->db->join('oa_areas as a3', 'a.area = a3.area_id', 'left');
-        $this->db->join('oa_user as u', 's.user_id = u.user_id');
-        $this->db->join('yjy_service_info as service_info', 'service_info.id = s.service_id', 'left');
+//        $this->db->join('oa_user as u', 's.user_id = u.user_id');
+//        $this->db->join('yjy_service_info as service_info', 'service_info.id = s.service_id', 'left');
         $this->db->order_by('s.id desc');
 //        $this->db->limit(10);
 
@@ -45,6 +45,7 @@ class OA_Appointment extends CI_Model
                 $this->db->where('state', 2000);
                 break;
             case 'all':
+                $this->db->where('state !=', -1);
                 break;
             default:
                 break;
