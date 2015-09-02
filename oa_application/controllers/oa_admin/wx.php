6f8f75a8ffda6c762eddedbb78d554d0cd85d82e
cheaboar <826802085@qq.com>
2015-09-02 15:25:40 +0800

    修改微信接口

--- oa_application/controllers/oa_admin/wx.php
+++ oa_application/controllers/oa_admin/wx.php
@@ -48,12 +48,13 @@ class Wx extends OA_Controller
 
         $server->on('event','Click',function($click){//点击事件
             switch($click['EventKey']){
-                case 'Getservice':
-                    return Message::make('text')->content('你点击了在线客服按钮');break;
-                case '开发中':
-                    return Message::make('text')->content('点击事件正在开发中');break;
+                case 'CUSTOMER_SERVICE':
+                    Message::make('text')->content('请等待，客服马上就来了!');
+                    return Message::make('transfer');
+                    break;
+
                 default:
-                    return Message::make('text')->content('点击事件正在开发中');break;
+                    return Message::make('text')->content('开发中');break;
             }
         });
 
@@ -61,20 +62,13 @@ class Wx extends OA_Controller
         $server->on('message','text',function($message) {
             log_message('info',$message);
             switch($message['Content']){
-                case '1':
-                    return Message::make('text')->content('小程已经很努力啦！');break;
-
-                case 'openid':
-                    return Message::make('text')->content($message['FromUserName']);
-                    break;
-
                 case '客服':
                     Message::make('text')->content('请等待，客服马上就来了!');
                     return Message::make('transfer');
                     break;
                 case 'wind':
                     return $this->cheng();
-                break;
+                    break;
                 default:
             }
         });
@@ -84,46 +78,6 @@ class Wx extends OA_Controller
             return Message::make('text')->content('我们已经收到您发送的图片！');
         });
         echo $server->serve();//接入微信公众帐号
-    }
-
-    /*
-     * 发送模版信息
-     *$userId string
-     *$templateId string //微信模版id
-     * $url 模版跳转链接，可以设置为空
-     * $data array
-     */
-    public function templateSend($userId,$templateId,$url,$data){
-        $notice = new Notice($this->appId, $this->appsecret);
-        $res  = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($userId)->send();
-        return $res;
-    }
-
-    /*
-     *授权页面获取用户信息
-    */
-    public function getUserMsg(){
-        $tourl = $this->input->get('url');
-        $auth = new Auth($this->appId,$this->appsecret);
-        $userMsg = $auth->authorize($to = null, $scope = 'snsapi_userinfo', $state = 'STATE');
-        $msg = '?openid='.$userMsg['openid'].'&nickname='.$userMsg['nickname'].'&sex='.$userMsg['sex'].'&language='.$userMsg['language'].'&city='.$userMsg['city'].'&province='.$userMsg['province'].'&country='.$userMsg['country'].'&headimgurl='.$userMsg['headimgurl'].'&privilege='.$userMsg['privilege'];
-        redirect($tourl.$msg);
-    }
-
-    //测试模板发送
-    public function cheng(){
-        $openid1 = $this->windOpenid;
-        $templateid1 = 'nP4fAUPrJc-r4RLHmSytRAfsc7EfvYxe-uQp-F-6Sik';
-        $url1 = 'http://www.baidu.com';
-        $data1 = array(
-            "first"    => "您好，您的最近一期健康报告已生成，详情如下。",
-            "keynote1" => "2014年7月21日 18:36",
-            "keynote2" => "各项指标均不正常，请进一步检查。总体评分为零",
-            "keynote3"   => "别再纠结了！",
-            "keynote4"   => "快点拨打120吧！",
-            "remark"   => "点击查看更多",
-        );
-        $this->templateSend($openid1,$templateid1,$url1,$data1);
     }
 }
 ?>
